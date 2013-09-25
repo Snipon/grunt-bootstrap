924a137c4405773880fda8a15b41e3e0d444a355
Simon Larsson <simon@reload.dk>
2013-09-25 14:38:28 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Test</title>
+  <link href="bower_components/bootstrap/less/bootstrap.less" rel="stylesheet/less" type="text/css">
+  <link href="less/style.less" rel="stylesheet/less" type="text/css">
+</head>
+<body class="woot">
+  <div class="page">
+    <header>
+      <div class="container">
+        <div class="inner row">
+          <div class="content col-lg-12">
+            <span class="label label-info">Branding</span>
+            <h1 class="site-title">
+              <figure class="site-logo"><img src="animal.svg" height="100" /></figure>
+              Sandbox
+            </h1>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="main">
+      <div class="container">
+        <div class="inner row">
+          <div class="content col-lg-9">
+            <span class="label label-info">Main</span>
+          </div>
+          <div class="content col-lg-3">
+            <span class="label label-info">Aside</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <div class="container">
+        <div class="inner row ">
+          <div class="content col-lg-12">
+            <span class="label label-info">Closure</span>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+  <script data-main="js/main" src="bower_components/requirejs/require.js"></script>
+</body>
+</html>
